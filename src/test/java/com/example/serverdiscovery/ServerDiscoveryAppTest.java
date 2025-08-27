d5d32ca4a110d6b2c241a1db57b560d2e55e7448
Erick <erickperez091@hotmail.com>
2025-08-27 15:48:00 -0600

    no message

--- src/test/java/com/example/serverdiscovery/ServerDiscoveryAppTest.java
+++ src/test/java/com/example/serverdiscovery/ServerDiscoveryAppTest.java
@@ -1,8 +1,8 @@
 package com.example.serverdiscovery;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple ServerDiscoveryApp.
@@ -13,6 +13,6 @@ public class ServerDiscoveryAppTest {
      */
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue( true );
+        Assertions.assertTrue( true );
     }
 }
